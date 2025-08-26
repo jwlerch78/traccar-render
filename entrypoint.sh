62d2ab3c495f762e92eab60150d24bec853be777
jwlerch78 <jwlerch@gmail.com>
2025-08-26 18:20:02 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
 
-# Start Traccar using bundled Java
-exec /opt/traccar/jre/bin/java -jar /opt/traccar/tracker-server.jar conf/traccar.xml
+# Ensure TRACCAR_PORT is set
+export TRACCAR_PORT=${PORT}
+
+# Start Traccar with host 0.0.0.0
+exec /opt/traccar/jre/bin/java -Dtraccar.host=0.0.0.0 -jar /opt/traccar/tracker-server.jar conf/traccar.xml
