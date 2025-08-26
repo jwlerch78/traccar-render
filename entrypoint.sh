540b1d59fb0cee039765a11a6a1917cbd7780c47
jwlerch78 <jwlerch@gmail.com>
2025-08-26 18:14:00 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -e
 
-# Replace the default port with Render's assigned port
-sed -i "s/<port>8082<\/port>/<port>${PORT}<\/port>/" /opt/traccar/conf/traccar.xml
-
-# Start Traccar
+# Start Traccar using bundled Java
 exec /opt/traccar/jre/bin/java -jar /opt/traccar/tracker-server.jar conf/traccar.xml
