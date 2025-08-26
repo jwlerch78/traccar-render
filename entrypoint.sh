1cd14bcaa610ed5e62cdec4e223f55d70450386d
jwlerch78 <jwlerch@gmail.com>
2025-08-26 18:02:38 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-# Replace hardcoded 8082 with Render’s assigned port
+# Update port inside config
 sed -i "s/<port>8082<\/port>/<port>${PORT}<\/port>/" /opt/traccar/conf/traccar.xml
 
-# Start Traccar
-exec /opt/traccar/bin/traccar run
+# Run the original start script (the image defaults to this)
+exec ./traccar.run
