ec1313b4c76e9a7482eb69dec91bf5d77b6bd411
jwlerch78 <jwlerch@gmail.com>
2025-08-27 09:32:00 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
+set -e
+
 echo "==== ENTRYPOINT START ===="
 
-PORT=${PORT:-10000}
+# Use Render's assigned PORT, default to 8082 if not set
+PORT=${PORT:-8082}
 echo "Render assigned PORT = $PORT"
 
-# Update XML so web.port matches Render’s PORT
-sed -i "s|<entry key='web.port'>.*</entry>|<entry key='web.port'>$PORT</entry>|" /opt/traccar/conf/traccar.xml
+# Patch traccar.xml to bind the web server to $PORT
+if [ -f /opt/traccar/conf/traccar.xml ]; then
+  sed -i "s|<entry key=\"web.port\">.*</entry>|<entry key=\"web.port\">${PORT}</entry>|" /opt/traccar/conf/traccar.xml
+  echo "Updated /opt/traccar/conf/traccar.xml with PORT=$PORT"
+else
+  echo "⚠️ No traccar.xml found!"
+fi
 
-echo "Launching Traccar in foreground on 0.0.0.0:$PORT..."
-exec /opt/traccar/bin/traccar run
+echo "Launching Traccar on 0.0.0.0:$PORT..."
+exec sh /opt/traccar/bin/traccar run
