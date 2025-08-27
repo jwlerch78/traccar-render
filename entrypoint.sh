6010f5d474bec3ca5fd7a1b9b3995487ac4f14e7
jwlerch78 <jwlerch@gmail.com>
2025-08-26 21:41:00 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,34 @@
+
 #!/bin/sh
 set -e
 
-# Replace default web port with Render-assigned port
+echo "PORT = $PORT"
+
+# Patch XML
 sed -i "s/<web.port>8082<\/web.port>/<web.port>${PORT}<\/web.port>/" /opt/traccar/conf/traccar.xml
 
+# Start Traccar in background for debug
+/opt/traccar/jre/bin/java -Dtraccar.host=0.0.0.0 -jar /opt/traccar/tracker-server.jar conf/traccar.xml &
+
+# Give it a few seconds to start
+sleep 5
+
+# Show listening ports
+netstat -tulpn
+
+# Keep container alive
+wait
+
+
+
+
+
+
+#!/bin/sh
+# set -e
+
+# Replace default web port with Render-assigned port
+# sed -i "s/<web.port>8082<\/web.port>/<web.port>${PORT}<\/web.port>/" /opt/traccar/conf/traccar.xml
+
 # Force binding to all interfaces
-exec /opt/traccar/jre/bin/java -Dtraccar.host=0.0.0.0 -jar /opt/traccar/tracker-server.jar conf/traccar.xml
+#exec /opt/traccar/jre/bin/java -Dtraccar.host=0.0.0.0 -jar /opt/traccar/tracker-server.jar conf/traccar.xml
