46739057d860bdfb145144135250ebad046889a1
jwlerch78 <jwlerch@gmail.com>
2025-08-27 09:28:43 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,11 @@
 #!/bin/sh
-set -e
-
 echo "==== ENTRYPOINT START ===="
+
+PORT=${PORT:-10000}
 echo "Render assigned PORT = $PORT"
 
-TRACCAR_CONF=/opt/traccar/conf/traccar.xml
+# Update XML so web.port matches Render’s PORT
+sed -i "s|<entry key='web.port'>.*</entry>|<entry key='web.port'>$PORT</entry>|" /opt/traccar/conf/traccar.xml
 
-# Backup XML
-cp $TRACCAR_CONF $TRACCAR_CONF.bak
-
-# Update web port in XML as a fallback
-sed -i "s#<web.port>[0-9]\+</web.port>#<web.port>${PORT}</web.port>#" $TRACCAR_CONF
-
-echo "Launching Traccar on 0.0.0.0:$PORT..."
-
-# Start Traccar with forced web port and host binding
-/opt/traccar/jre/bin/java -Dtraccar.host=0.0.0.0 -Dtraccar.web.port=$PORT -jar /opt/traccar/tracker-server.jar $TRACCAR_CONF &
-
-sleep 5
-
-echo "=== LISTENING PORTS ==="
-netstat -tulpn || echo "netstat command not available"
-
-wait
+echo "Launching Traccar in foreground on 0.0.0.0:$PORT..."
+exec /opt/traccar/bin/traccar run
